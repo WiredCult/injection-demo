a493cd193d5067e00455f0d6e3ea87466ac20156
Azzy <liminalfox.mon@proton.me>
2026-02-08 20:27:06 -0400

    postgresdb setup and seeded

---
+++ init/seed.sql
@@ -0,0 +1,20 @@
+CREATE TABLE users (
+    username       TEXT NOT NULL,
+    password    TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE comments (
+    id  SERIAL PRIMARY KEY,
+    username    TEXT NOT NULL,
+    comment     TEXT NOT NULL
+);
+
+INSERT INTO users (username, password) VALUES
+('admin', 'password'),
+('jabooboo',   'jabootyplz');
+
+INSERT INTO comments (username, comment) VALUES
+('squatCobbler44',   'HHHHHHHHHHHHHHHHHHHHHHHHHHH'),
+('Bencil-Sharpino',   'This is utterly ridiculous on so many levels. You expect me to believe that a cob of this size is obtainable without implants or medication? The damage we are doing to men in the cornosphere with these desperate displays of shuckery are absolutely unjust. My lawyers will be in touch.'),
+('papakernal',   'can i dm u'),
+('bishucksualgurl98',   'oh GOD thats a big harvest. certainly no famine this year xoxo');
